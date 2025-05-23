728502651aadd55956cccb493d42339ee9cd1e0e
b0jun <bjkim0228@naver.com>
2025-05-23 14:41:11 +0900

    feat: [BE] Auth Guard 적용

--- inflearn-fullstack-clone/backend/src/app.module.ts
+++ inflearn-fullstack-clone/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 @Module({
   // * ConfigModule: NestJS에서 환경변수(.env) 파일을 로드하고 애플리케이션 전체에서 사용할 수 있게 만드는 설정
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
