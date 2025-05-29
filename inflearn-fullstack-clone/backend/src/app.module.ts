813e10b356d90ad14e50d5c4bb99e414fbbdebb1
b0jun <bjkim0228@naver.com>
2025-05-29 15:47:39 +0900

    feat: [BE] 동영상 업로드 API 추가

--- inflearn-fullstack-clone/backend/src/app.module.ts
+++ inflearn-fullstack-clone/backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { CoursesModule } from './courses/courses.module';
 import { LecturesModule } from './lectures/lectures.module';
 import { SectionsModule } from './sections/sections.module';
 import { CategoriesModule } from './categories/categories.module';
+import { MediaModule } from './media/media.module';
 @Module({
   // * ConfigModule: NestJS에서 환경변수(.env) 파일을 로드하고 애플리케이션 전체에서 사용할 수 있게 만드는 설정
   imports: [
@@ -18,6 +19,7 @@ import { CategoriesModule } from './categories/categories.module';
     LecturesModule,
     SectionsModule,
     CategoriesModule,
+    MediaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
